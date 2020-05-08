f9b634c4b7ef1c481f2d6decbbb9085c7f4e10fe
alanamacedo <macedo.at4@gmail.com>
2020-05-07 18:33:07 -0700

    getting help

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -25,4 +25,15 @@
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
   });
+
+  $('time-control input').on('input', function (event) {
+  });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration - player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control input').val(percent);
+  }, 1000);
 }
